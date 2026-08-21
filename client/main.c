21ac47e9fed0e4ed5e2b6b71d22443a7b031fb15
aanand1110 <anandpatel4411@gmail.com>
2026-08-21 14:48:19 -0500

    implemented basic TCP socket connections.

--- client/main.c
+++ client/main.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
+int main(){
+	// 1. Create socket
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	// 2. Describe the server
+	struct sockaddr_in server_addr = {0};
+	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(12345);
+	int ip_conversion = inet_pton(AF_INET, "127.0.0.1",&server_addr.sin_addr);
+	if (ip_conversion != 1){
+		printf("invalid ip or addressfamily\n");
+		exit(-1);
+	}
+
+	// 3. Connect to localhost:12345
+	int connect_ret_value= connect(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	if(connect_ret_value == -1){
+		printf("Connection failed\n");
+		exit(connect_ret_value);
+	}
+
+	// 4. Send data
+	char msg[] = "Say Hello\n";
+
+	send(sockfd, msg, strlen(msg), 0);
+
+	// 5. Receive response
+	char buffer[1024];
+
+	int n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+
+	if (n > 0) {
+        	buffer[n] = '\0';
+        	printf("Server: %s\n", buffer);
+	}else if(n == 0){
+		printf("Server connection closed.");
+	}
+
+	// 6. Close socket
+	int close_ret_val = close(sockfd);
+	if (close_ret_val == 0){
+		printf("Connection closed sussfully.\n");
+	}else{
+		printf("Something went wrong while closing the connection.");
+	}
+
+	return 0;
+}
--- server/main.c
+++ server/main.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
+int main(){
+	// 1. Create socket
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// 2. Describe the server address
+	struct sockaddr_in server_addr = {0};
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(12345);
+
+	int ip_conversion = inet_pton(AF_INET, "127.0.0.1",&server_addr.sin_addr);
+	if(ip_conversion != 1){
+		printf("It is an invalid ip or address family.\n");
+		exit(-1);
+	}
+
+	// 3. Bind socket to localhost:12345
+	int bind_ret = bind(server_fd,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	if(bind_ret == -1){
+		perror("Some problem binding with the port.\n");
+		exit(-1);
+	}
+
+	// 4. Listen for incoming connections
+	int listen_ret = listen(server_fd, 5);
+	if(listen_ret == -1){
+		perror("The listen function failed.\n");
+	}
+
+	printf("Server listening on 127.0.0.1:12345...\n");
+	while(1){
+		// 5. Accept a client connection
+		int client_fd = accept(server_fd, NULL, NULL);
+
+		printf("Client connected!\n");
+
+		// 6. Receive data from client
+		char buffer[1024];
+
+		int n = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+		if (n > 0) {
+        		buffer[n] = '\0';
+        		printf("Client: %s\n", buffer);
+		}
+
+		// 7. Send response to client
+		char msg[] = "Hello";
+		send(client_fd, msg, strlen(msg), 0);
+		// 8. Close client connection
+		//close(client_fd);
+	}
+
+	// 9. Close server socket
+	close(server_fd);
+
+	return 0;
+}
